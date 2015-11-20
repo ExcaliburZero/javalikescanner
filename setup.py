2697818a21145786e3a02c58cb5c2112fab4d3a5
ExcaliburZero <cwellsny@nycap.rr.com>
2015-11-20 17:58:54 -0500

    Add basic package structure
    Add a sub-directory for a package to contain the modules in the library. Also add a setup script to be used when packaging the library.

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""The script which is used when packaging the library."""
+from setuptools import setup
+
+setup(name='javalikescanner',
+      version='0.1',
+      description='A Python library which includes a class which functions' + \
+            'simmilarly to the default Scanner class of Java.',
+      url='https://github.com/ExcaliburZero/javalikescanner',
+      author='Christopher Randall Wells',
+      author_email='cwellsny@nycap.rr.com',
+      license='MIT',
+      packages=['javalikescanner'])
