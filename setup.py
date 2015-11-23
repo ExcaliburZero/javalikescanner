d2fe585cbcf33c0c5c5a5e1246450d4b33ca45e3
ExcaliburZero <cwellsny@nycap.rr.com>
2015-11-23 10:44:01 -0500

    Add more packaging information
    Add more information to the setup file for use in packaging the library.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup( \
       version='0.1',
       description='A Python library which includes a class which functions' + \
             'simmilarly to the default Scanner class of Java.',
+      long_description=open('README.rst').read(),
       url='https://github.com/ExcaliburZero/javalikescanner',
       author='Christopher Randall Wells',
       author_email='cwellsny@nycap.rr.com',
@@ -13,5 +14,19 @@ setup( \
       packages=['javalikescanner'],
       install_requires=[
           'numpy',
+      ],
+      include_package_data=True,
+      package_data={
+          '': 'LICENSE'
+      },
+
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Operating System :: OS Independent',
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Software Development :: Libraries :: Python Modules'
       ]
      )
