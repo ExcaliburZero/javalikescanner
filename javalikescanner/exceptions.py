5f97c4fcd5c1fe99a936e3c1f83241279d566124
ExcaliburZero <cwellsny@nycap.rr.com>
2015-12-10 20:11:17 -0500

    Fix exception docstring line length

--- javalikescanner/exceptions.py
+++ javalikescanner/exceptions.py
@@ -2,12 +2,14 @@
 
 class NoSuchElementException(Exception):
 	"""
-	An exception which is raised when a token is attempted to be accessed and there is no next token in the scanner.
+	An exception which is raised when a token is attempted to be accessed and there is no next token
+	in the scanner.
 	"""
 	pass
 
 class InputMismatchException(Exception):
 	"""
-	An exception which is raised when a specific type of token is requested, such as an integer, and the next token in the scanner is not of that type.
+	An exception which is raised when a specific type of token is requested, such as an integer, and
+	the next token in the scanner is not of that type.
 	"""
 	pass
