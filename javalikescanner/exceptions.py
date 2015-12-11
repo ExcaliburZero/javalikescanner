80e15545a399cba3941797ec0ee8276223b8fdd6
ExcaliburZero <cwellsny@nycap.rr.com>
2015-12-10 20:09:44 -0500

    Add docstrings for exceptions

--- javalikescanner/exceptions.py
+++ javalikescanner/exceptions.py
@@ -1,7 +1,13 @@
 """A module which defines the eceptions thrown by the JavaLikeScanner class."""
 
 class NoSuchElementException(Exception):
+	"""
+	An exception which is raised when a token is attempted to be accessed and there is no next token in the scanner.
+	"""
 	pass
 
 class InputMismatchException(Exception):
+	"""
+	An exception which is raised when a specific type of token is requested, such as an integer, and the next token in the scanner is not of that type.
+	"""
 	pass
